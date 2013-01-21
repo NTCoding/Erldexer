d8fb3ebd4ea906605697626f82dcd36467076027
Nick <nick@ntcoding.co.uk>
2013-01-21 12:19:24 +0000

    batcher now batches and published to queue

--- src/services/batcher.erl
+++ src/services/batcher.erl
@@ -4,6 +4,8 @@
 
 -include_lib("lib/amqp_client/include/amqp_client.hrl").
 
+-define(BATCHSIZE, 50).
+
 batch() -> 
 	spawn(fun() -> begin_batching() end).
 
@@ -23,17 +25,32 @@ begin_batching() ->
 	loop(Channel, []).
 
 
+loop(Channel, Batch) when erlang:length(Batch) >= 50 -> 
+	publish(Batch, Channel),
+	loop(Channel, []);
+
 loop(Channel, Batch) ->
 	receive
 		{#'basic.deliver'{}, #amqp_msg{payload = Payload}} ->
 			io:format(" Received a message. About to batch it ~n"),
 			Track = [binary_to_term(Payload)],
 			NewBatch = lists:append(Batch, Track),
-			io:format("current batch: ~n~p~n", [NewBatch]),
 			loop(Channel, NewBatch)			
 	after 30 ->
 		loop(Channel, Batch)
 	end.
+
+
+publish(Batch, Channel) -> 
+	Binary = term_to_binary(Batch),
+	Message = #amqp_msg{payload = Binary },
+	amqp_channel:call(Channel, #'queue.declare'{queue = <<"upsertbatches">>}),
+	Publish = #'basic.publish'{ exchange = <<"">>, routing_key = <<"upsertbatches">>},
+	amqp_channel:cast(Channel, Publish, Message),
+	io:format("Batcher published a batch ~n").
+	
+	
+
 
 	
 
