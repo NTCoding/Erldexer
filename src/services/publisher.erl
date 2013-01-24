fc166ee70a2109c4221a4b7f4a21774211d3b001
Nick <nick@ntcoding.co.uk>
2013-01-24 14:32:30 +0000

    publisher now sends ack

--- src/services/publisher.erl
+++ src/services/publisher.erl
@@ -16,21 +16,21 @@ channel() ->
 %% publishing
 publish(Channel) ->
 	Batch = next_batch(Channel),
-	publish_batch(Batch),
+	publish_batch(Batch, Channel),
 	timer:sleep(1000 * 30),
 	publish(Channel).	
 
-publish_batch(no_batch) ->
+publish_batch(no_batch, Channel) ->
 	io:format("No batches available to publish. Will look again in 30 seconds~n");
 
-publish_batch(Batch) -> 
-	io:format("Published batch to data store. Next batch in 30 seconds~n").
+publish_batch({Content, Tag}, Channel) -> 
+	amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+	io:format("Published batch to data store & sent ack to queue. Next batch in 30 seconds~n").
 
 
 %% getting batch
 next_batch(Channel) ->
 	PriorityBatch = read_priority_batch(Channel),
-	io:format("Got priority batch ~p~n", [PriorityBatch]),
 	next_batch(PriorityBatch, Channel).
 
 next_batch(no_priority_batch_available, Channel) -> read_normal_batch(Channel);
@@ -49,7 +49,7 @@ read_batch_or_default(Channel, Queue, Default) ->
 	amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
 	case amqp_channel:call(Channel, #'basic.get'{queue = Queue}) of
 
-			{#'basic.get_ok'{}, Content} ->	Content;
+			{#'basic.get_ok'{delivery_tag = Tag}, Content} ->	{Content, Tag};
 
 			#'basic.get_empty'{} -> default
 
