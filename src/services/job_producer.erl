886bedfbe61ecb408e5398afbd17d94664827833
Nick <nick@ntcoding.co.uk>
2013-01-24 14:35:37 +0000

    jobs are now assigned to a shop

--- src/services/job_producer.erl
+++ src/services/job_producer.erl
@@ -2,27 +2,27 @@
 
 -include_lib("lib/amqp_client/include/amqp_client.hrl").
 
--export([produce/1, produce_priority_jobs/1]).
+-export([produce/2, produce_priority_jobs/2]).
 
-produce(Amount) -> 
+produce(Amount, Shop) -> 
 	{Connection, Channel} = open_channel(),
-	produce(Amount, Channel, Connection, normal_priority).
+	produce(Amount, Channel, Connection, normal_priority, Shop).
 
 
-produce_priority_jobs(Amount) -> 
+produce_priority_jobs(Amount, Shop) -> 
 	{Connection, Channel} = open_channel(),
-	produce(Amount, Channel, Connection, high_priority).
+	produce(Amount, Channel, Connection, high_priority, Shop).
 	
 
-produce(Amount, Channel, Connection, Priority) when Amount > 0 ->
+produce(Amount, Channel, Connection, Priority, Shop) when Amount > 0 ->
 	Publish = #'basic.publish'{ exchange = <<"">>, routing_key = <<"upsertjobs">>},
-	Binary = term_to_binary(track_details_for(Amount, Priority)),
+	Binary = term_to_binary(track_details_for(Amount, Priority, Shop)),
 	Message = #amqp_msg{payload = Binary },
 	amqp_channel:cast(Channel, Publish, Message),
 	io:format("produced job. ~p left to go~n", [Amount - 1]),
-	produce(Amount - 1, Channel, Connection, Priority);
+	produce(Amount - 1, Channel, Connection, Priority, Shop);
 	
-produce(0, Channel, Connection, _) -> 
+produce(0, Channel, Connection, _, _) -> 
 	io:format("Completed producing jobs. Closing connection~n"),
     ok = amqp_channel:close(Channel),
     ok = amqp_connection:close(Connection).
@@ -35,7 +35,7 @@ open_channel() ->
 	{Connection, Channel}.
 
 
-track_details_for(Number, Priority) ->
+track_details_for(Number, Priority, Shop) ->
 	{
 		{priority, Priority},
 		{details,
@@ -43,7 +43,8 @@ track_details_for(Number, Priority) ->
 			{release, "Release " ++ Number},
 			{artist, "Artist " ++ Number},
 			{price, "99"},
-			{released, "01/01/2012"}
+			{released, "01/01/2012"},
+			{shop, Shop}
 		}		
 	}.
 	
